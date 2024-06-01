e98fc2eab59e5205fa1a079f27bcc9997f8444bb
Zehui2020 <zehui2020@gmail.com>
2024-06-01 22:03:22 +0800

    Added stun grenade model

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -10,7 +10,7 @@ public class Item : ScriptableObject
     public enum Rarity { Common, Uncommon, Legendary, Special };
     public Rarity itemRarity;
 
-    public enum ItemType { HDHUD, AdrenalineShot, Gasoline, CrudeKnife, RitualSickle, HoloDie, BundleOfDynamite, ColdOne, WarmOne };
+    public enum ItemType { HDHUD, AdrenalineShot, Gasoline, CrudeKnife, RitualSickle, HoloDie, BundleOfDynamite, ColdOne, WarmOne, StunGrenade };
     public ItemType itemType;
 
     public enum ItemCatagory { Damage, Healing, Utility };
